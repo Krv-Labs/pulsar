246710f9b65cbe7fde3a8387ac313497b7bef188
jeremy-wayland <jeremy.don.wayland@gmail.com>
2026-03-27 10:38:20 +0100

    feat: add TemporalCosmicGraph for longitudinal time-series data analysis

--- pulsar/__init__.py
+++ pulsar/__init__.py
@@ -6,9 +6,12 @@ from pulsar._pulsar import (
     BallMapper,
     ball_mapper_grid,
     accumulate_pseudo_laplacians,
+    accumulate_temporal_pseudo_laplacians,
+    py_normalize_temporal_laplacian as normalize_temporal_laplacian,
     CosmicGraph,
 )
 from pulsar.pipeline import ThemaRS
+from pulsar.temporal import TemporalCosmicGraph
 from pulsar.hooks import (
     label_points,
     membership_matrix,
@@ -18,15 +21,25 @@ from pulsar.hooks import (
 )
 
 __all__ = [
+    # Preprocessing
     "impute_column",
     "StandardScaler",
+    # Dimensionality reduction
     "PCA",
     "pca_grid",
+    # Ball Mapper
     "BallMapper",
     "ball_mapper_grid",
+    # Pseudo-Laplacian accumulation
     "accumulate_pseudo_laplacians",
+    "accumulate_temporal_pseudo_laplacians",
+    "normalize_temporal_laplacian",
+    # Cosmic Graph
     "CosmicGraph",
+    "TemporalCosmicGraph",
+    # Pipeline
     "ThemaRS",
+    # Hooks / utilities
     "label_points",
     "membership_matrix",
     "cosmic_clusters",
